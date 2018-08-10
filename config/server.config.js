3279d775172e0c0be3835f633f1d5e1a68de6ee8
siver <2278254874@qq.com>
2018-08-10 11:11:10 +0800

    服务器迁移

--- config/server.config.js
+++ config/server.config.js
@@ -1,6 +1,6 @@
 /**
  * Created by wushuang on 2018/7/23.
  */
-const Host = "116.62.40.165:8889";
 // const Host = "127.0.0.1:8889";
+const Host = "120.79.64.251:8889";
 export const rootURL = "http://"+Host+"/";
