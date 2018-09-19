7c116e37493e73c565602f6a5777e334a8c9d3d3
siver <2278254874@qq.com>
2018-09-19 10:45:34 +0800

    fix bugs

--- config/server.config.js
+++ config/server.config.js
@@ -1,6 +1,6 @@
 /**
  * Created by wushuang on 2018/7/23.
  */
-const Host = "127.0.0.1:8889";
-// const Host = "120.79.232.221:8889";
+// const Host = "127.0.0.1:8889";
+const Host = "120.79.232.221:8889";
 export const rootURL = "http://"+Host+"/";
